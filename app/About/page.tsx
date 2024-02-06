75be4c25498f139e6b363aef520980a053a92f13
Huntergs16 <samoyhun@msu.edu>
2024-02-05 21:22:21 -0600

    Made Shoutout a core component

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import Link from "next/link";
+import ShoutOut from "@/components/Shoutout";
 
 export default function About() {
   return (
@@ -45,29 +44,5 @@ export default function About() {
         </div>
       </div>
     </div>
-  );
-}
-
-export function ShoutOut({
-  name,
-  image,
-  description,
-  link,
-}: {
-  name: string;
-  image: string;
-  description: string;
-  link: string;
-}) {
-  return (
-    <Link
-      href={link}
-      className='flex flex-col items-center gap-4 cursor-pointer'
-      target='_blank'
-    >
-      <Image src={image} alt={name} width={200} height={200} />
-      <p className='text-2xl'>{name}</p>
-      <p className='text-xl'>{description}</p>
-    </Link>
   );
 }
---
+++ components/Shoutout.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ShoutOut({
+  name,
+  image,
+  description,
+  link,
+}: {
+  name: string;
+  image: string;
+  description: string;
+  link: string;
+}) {
+  return (
+    <Link
+      href={link}
+      className='flex flex-col items-center gap-4 cursor-pointer'
+      target='_blank'
+    >
+      <Image src={image} alt={name} width={200} height={200} />
+      <p className='text-2xl'>{name}</p>
+      <p className='text-xl'>{description}</p>
+    </Link>
+  );
+}
